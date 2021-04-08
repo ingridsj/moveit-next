f1384304da7c10b005f9805cb64a991c35bc0735
ingridsj <ingridsjalmeida@gmail.com>
2021-04-08 16:26:07 -0300

    Update ChallengesContext.tsx

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,16 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-export const challengesContext = createContext({});
+export const ChallengesContext = createContext({});
 
+export function ChallengesProvider({ children }) {
+  const [level, setLevel] = useState(1);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+  return (
+    <ChallengesContext.Provider value={{level, levelUp}}>
+      { children }
+    </ChallengesContext.Provider>
+  );
+}
