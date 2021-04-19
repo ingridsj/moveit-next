9740dc213f8ea9ac743eda978857e88729ef7f25
ingridsj <ingridsjalmeida@gmail.com>
2021-04-18 23:21:52 -0300

    adding reset challenge button

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,16 +1,29 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox () {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completedChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext)
+
+
+  function handleChallengeSucceeded(){
+    completedChallenge()
+    resetCountdown()
+  }
+
+  function handleChallengeFailed(){
+    resetChallenge()
+    resetCountdown()
+  }
 
   return (
     <div className={ styles.challengeBoxContainer }>
       { activeChallenge ? (
         <div className={ styles.challengeActive }>
-          <header>Ganhe { activeChallenge.amount }</header>
+          <header>Ganhe { activeChallenge.amount } xp</header>
 
           <main>
             <img src={`icons/${activeChallenge.type}.svg`} alt=""/>
@@ -22,14 +35,14 @@ export function ChallengeBox () {
             <button 
             type="button"
             className={ styles.challengeFailedButton }
-            onClick = { resetChallenge }
+            onClick = { handleChallengeFailed }
             >
               Falhei
             </button>
             <button 
             type="button"
             className ={ styles.challengeSucceededButton }
-            //onClick = 
+            onClick = { handleChallengeSucceeded }
             >
               Completei
             </button>
